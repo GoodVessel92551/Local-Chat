c555794c78c652cf3a0f528708b5c59c956845fa
GoodVessel92551 <robininness@gmail.com>
2024-06-26 11:12:54 +0100

    refactor: Update CSS file paths for guide and index pages

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const welcome = document.getElementById("welcome");
 const logo = document.getElementById("logo");
 const clear_button = document.getElementById("clear_button");
 
+
+
 let running = false;
 let message_id = 0;
 let called = false;
@@ -13,6 +15,9 @@ let messages = [];
 let model = null;
 const ai_guide = document.getElementById('ai_guide');
 
+const url_find = (text) => {
+    return text.match(urlPattern);
+}
 
 window.addEventListener("load", async () => {
     const hasAI = window.ai != null;
@@ -163,12 +168,15 @@ const ai_call = async (userInput, messages) => {
     position.classList.add("genning");
     position.innerHTML = `<svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M8.664 15.735c.245.173.537.265.836.264v-.004a1.442 1.442 0 0 0 1.327-.872l.613-1.864a2.872 2.872 0 0 1 1.817-1.812l1.778-.578a1.443 1.443 0 0 0-.052-2.74l-1.755-.57a2.876 2.876 0 0 1-1.822-1.823l-.578-1.777a1.446 1.446 0 0 0-2.732.022l-.583 1.792a2.877 2.877 0 0 1-1.77 1.786l-1.777.571a1.444 1.444 0 0 0 .017 2.734l1.754.569a2.887 2.887 0 0 1 1.822 1.826l.578 1.775c.099.283.283.528.527.7Zm-.374-4.25a4.054 4.054 0 0 0-.363-.413h.003a4.394 4.394 0 0 0-1.72-1.063l-1.6-.508 1.611-.524a4.4 4.4 0 0 0 1.69-1.065 4.448 4.448 0 0 0 1.041-1.708l.515-1.582.516 1.587a4.374 4.374 0 0 0 2.781 2.773l1.62.522-1.59.515a4.379 4.379 0 0 0-2.774 2.775l-.515 1.582-.515-1.585a4.368 4.368 0 0 0-.7-1.306Zm8.041 9.297a1.123 1.123 0 0 1-.41-.549l-.328-1.007a1.293 1.293 0 0 0-.821-.823l-.991-.323A1.148 1.148 0 0 1 13 16.997a1.143 1.143 0 0 1 .771-1.08l1.006-.326a1.3 1.3 0 0 0 .8-.819l.324-.992a1.143 1.143 0 0 1 2.157-.021l.329 1.014a1.3 1.3 0 0 0 .82.816l.992.323a1.141 1.141 0 0 1 .039 2.165l-1.014.329a1.3 1.3 0 0 0-.818.822l-.322.989c-.078.23-.226.43-.425.57a1.14 1.14 0 0 1-1.328-.005Zm-1.03-3.783A2.789 2.789 0 0 1 17 18.708a2.794 2.794 0 0 1 1.7-1.7 2.813 2.813 0 0 1-1.718-1.708A2.806 2.806 0 0 1 15.3 17Z" fill="#ffffff"/></svg>Generating`
     const stream = model.promptStreaming(`Please respond as concise as possible to the conversation below. ${messages.map((message) => `${message.role}: ${message.content}`).join("\n")}\n user: ${userInput.trim()}\n assistant:`);
-    for await (const response of stream) {
-        script.textContent = response;
-        tokens += 1;
-        total = response;
-        scrollToBottom()
+    const generate = async () => {
+        for await (const response of stream) {
+            script.textContent = response;
+            tokens += 1;
+            total = response;
+            scrollToBottom()
+        }
     }
+    await generate();
     messages.push({ "role": "user", "content": prompt });
     messages.push({ "role": "assistant", "content": total });
     position.innerHTML = `<svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M4.53 12.97a.75.75 0 0 0-1.06 1.06l4.5 4.5a.75.75 0 0 0 1.06 0l11-11a.75.75 0 0 0-1.06-1.06L8.5 16.94l-3.97-3.97Z" fill="#ffffff"/></svg>Complete`
